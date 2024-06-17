9eb1c395ca8f36d962411838d1f8377694b3d722
GuyMor89 <guymor89@gmail.com>
2024-06-17 19:48:26 +0300

    Add renderTable function

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,2 +1,43 @@
 'use strict'
 
+function onInit() {
+    renderTable()
+}
+
+function renderTable() {
+    var table = document.querySelector('.table')
+
+    var bookHTML = getBooks().map((book, idx) =>
+        `<div class="title title${idx + 1}">${book.title}</div>
+         <div class="price price${idx + 1}">${book.price}</div>
+         <div class="actions actions${idx + 1}">
+                <div class="read read${idx + 1}">Read</div>
+                <div class="update update${idx + 1}">Update</div>
+                <div class="delete delete${idx + 1}">Delete</div>
+         </div>`
+    )
+    table.insertAdjacentHTML('beforeend', bookHTML.join(''))
+
+
+    var bookGridArea = getBooks().map((book, idx) =>
+        `"title${idx + 1} price${idx + 1} actions${idx + 1}"`
+    )
+    var currentGridAreas = getComputedStyle(table).gridTemplateAreas
+    var updatedGridAreas = currentGridAreas + bookGridArea.join('')
+
+    table.style.gridTemplateAreas = updatedGridAreas
+
+
+    var bookCSS = getBooks().map((book, idx) =>
+        `.title${idx + 1} { grid-area: title${idx + 1}}
+    .price${idx + 1} {grid-area: price${idx + 1}}
+    .actions${idx + 1} {grid-area: actions${idx + 1}}`
+    )
+
+    var styleSheet = document.createElement("style")
+    styleSheet.textContent = bookCSS.join('')
+    document.head.appendChild(styleSheet)
+
+}
+
+
--- js/book.service.js
+++ js/book.service.js
@@ -1,2 +1,33 @@
 'use strict'
 
+const gBooks = [
+    {
+        id: 1,
+        title: 'Harry Potter',
+        price: 120
+    },
+    {
+        id: 2,
+        title: 'Flowers for Algernon',
+        price: 75
+    },
+    {
+        id: 3,
+        title: 'Persuasion',
+        price: 90
+    },
+    {
+        id: 4,
+        title: 'A Song of Ice and Fire',
+        price: 200
+    },
+    {
+        id: 5,
+        title: 'Sherlock Holmes',
+        price: 50
+    }
+]
+
+function getBooks() {
+    return gBooks
+}
