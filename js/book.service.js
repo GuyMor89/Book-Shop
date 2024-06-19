4d8a823c0eb61459509395f4f6638fac8776b70b
GuyMor89 <guymor89@gmail.com>
2024-06-20 00:48:35 +0300

    Add demoArray to provide demo data

--- js/book.service.js
+++ js/book.service.js
@@ -1,7 +1,32 @@
 'use strict'
 
 
-var gBooks = loadFromStorage('bookArray')
+function loadDemoData() {
+
+    const demoBooks = [
+        {
+            id: 1,
+            title: 'Harry Potter',
+            price: 120
+        },
+        {
+            id: 2,
+            title: 'Flowers for Algernon',
+            price: 75
+        },
+        {
+            id: 3,
+            title: 'Persuasion',
+            price: 90
+        }
+    ]
+
+    if (!loadFromStorage('bookArray').length > 0) saveToStorage('bookArray', demoBooks)
+    return loadFromStorage('bookArray')
+}
+
+
+var gBooks = loadDemoData()
 
 function getBooks() {
     return gBooks
